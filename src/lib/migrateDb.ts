e38a05a41a61d819d549ee0264c4e04fd47104a2
TheAimHero <91832491+TheAimHero@users.noreply.github.com>
2024-02-07 14:57:38 +0530

    feat: drizzle migrate init

---
+++ src/lib/migrateDb.ts
@@ -0,0 +1,19 @@
+import { migrate } from 'drizzle-orm/libsql/migrator';
+import { client, db } from '@/server/db';
+
+async function main() {
+  console.log('Running migrations');
+  await migrate(db, { migrationsFolder: 'drizzle' });
+  console.log('Migrated successfully');
+  process.exit(0);
+}
+
+main()
+  .catch((e) => {
+    console.error('Migration failed');
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(() => {
+    client.close();
+  });
